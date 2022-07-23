003f38eb83be1745f920f52df9ca483142125089
uen0_ <sarasa4869@zoho.com>
2022-07-23 17:06:50 +0900

    [Update] color変更による処理を追加

--- Assets/Script/BaseMoveController.cs
+++ Assets/Script/BaseMoveController.cs
@@ -7,31 +7,53 @@ public enum MoveAction
     AdvanceMove = 1,//前進
     Attack = 2,//攻撃
     //Chase = 2,//追従
-    //RecessionMove = -1,//後退
-
+    RecessionMove = -1,//後退
+}
+public enum PlayerColor
+{
+    Blue = 0,
+    Red = 1
 }
 public class BaseMoveController : MonoBehaviour
 {
     /// <summary>移動速度</summary>
     [SerializeField] protected float _speed = 5;
 
-    /// <summary>HP</summary>
-    protected int _helthPoint = 5;
+    /// <summary>Level</summary>
+    protected int _level = 1;
 
     /// <summary>ActionのType</summary>
-    [SerializeField] public MoveAction _ActionType = MoveAction.AdvanceMove;
+    [SerializeField] public MoveAction _actionType = MoveAction.AdvanceMove;
+    /// <summary>PlayerのType</summary>
+    [SerializeField] public PlayerColor _playerType = PlayerColor.Blue;
 
-    [SerializeField] protected GameObject _target; 
-    
+    [SerializeField] protected GameObject[] _targets;
+
+    private Vector3 _thisPos;
+    private Vector3 _targetPos;
+
+    private float _currentTargetDistance = 0f;
+
     void Start()
     {
         TargetSerch();
-
     }
 
     private void TargetSerch()
     {
-        _target = GameObject.FindWithTag("BlueCore");
+        switch (_playerType)
+        {
+            case 0:
+                _targets = GameObject.FindGameObjectsWithTag("Red");
+                break;
+            case (PlayerColor)1:
+                _targets = GameObject.FindGameObjectsWithTag("Blue");
+                break;
+        }
+        foreach (var obj in _targets)
+        {
+            _targetPos =  obj.transform.position;
+        }
     }
 
     void Update()
@@ -41,11 +63,10 @@ public class BaseMoveController : MonoBehaviour
 
     private void ActionChange()
     {
-        switch (_ActionType)
+        switch (_actionType)
         {
             case MoveAction.AdvanceMove:
 
-                Vector3 _targetPos = _target.transform.position;
                 //y軸はプレイヤーと同じにする
                 _targetPos.y = transform.position.y;
                 // プレイヤーに向かせる
@@ -57,7 +78,34 @@ public class BaseMoveController : MonoBehaviour
                 break;
             case MoveAction.Attack:
                 Debug.Log("攻撃");
+               
                 break;
         }
     }
+
+    private void FixedUpdate()
+    {
+        LookDistanse(2);
+    }
+
+    /// <summary>
+    /// ターゲットとの距離が指定した値に達したらActionを変える
+    /// </summary>
+    /// <param name="value"></param>
+    private void LookDistanse(float value)
+    {
+        _thisPos = this.gameObject.transform.position;
+
+        _currentTargetDistance = Mathf.Sqrt(Mathf.Pow(_thisPos.x - _targetPos.x, 2) + Mathf.Pow(_thisPos.z - _targetPos.z, 2));
+
+        if (_currentTargetDistance <= value)
+        {
+            _actionType = MoveAction.Attack;
+        }
+        else
+        {
+            _actionType = MoveAction.AdvanceMove;
+        }
+    }
+
 }
