8680f64e272882a43f7fbdb2459f59b62b5b41d7
uen0_ <sarasa4869@zoho.com>
2022-07-24 23:36:30 +0900

    [Update] ターゲットを決める処理をBaseMoveControllerからTargetSerchControllerへ移植した

--- Assets/Script/Charactor/TargetSerchScript.cs
+++ Assets/Script/Charactor/TargetSerchScript.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerColor
+{
+    Blue = 0,
+    Red = 1
+}
+
 public class TargetSerchScript : MonoBehaviour
 {
+    [SerializeField] protected GameObject[] _targets;
+
+    /// <summary>Player‚ÌType</summary>
+    [SerializeField] public PlayerColor _playerType = PlayerColor.Blue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TargetSerch();
     }
 
     // Update is called once per frame
@@ -15,4 +25,22 @@ public class TargetSerchScript : MonoBehaviour
     {
         
     }
+
+    private void TargetSerch()
+    {
+        switch (_playerType)
+        {
+            case 0:
+                _targets = GameObject.FindGameObjectsWithTag("Red");
+                break;
+            case (PlayerColor)1:
+                _targets = GameObject.FindGameObjectsWithTag("Blue");
+                break;
+        }
+        //foreach (var obj in _targets)
+        //{
+        //    _targetPos = obj.transform.position;
+        //}
+    }
+
 }
