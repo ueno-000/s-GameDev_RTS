163bd5699bbbc5bbb97f796282053096e697a4fc
uen0_ <sarasa4869@zoho.com>
2022-10-10 23:47:51 +0900

    [Add] カメラの方向に向けるscript

---
+++ Assets/Script/LookCameraScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookCameraScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(Camera.main.transform);
+    }
+}
