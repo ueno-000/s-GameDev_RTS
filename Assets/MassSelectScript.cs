591be58fce8f1bb5bcfc4a5995a9bb6eec66a3d3
uen0_ <sarasa4869@zoho.com>
2022-10-11 23:54:12 +0900

    [Update] EventSystemでの制御

--- Assets/MassSelectScript.cs
+++ Assets/MassSelectScript.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MassSelectScript : MonoBehaviour
 {
-    private Vector3 _mouse;
-    private Vector3 _target;
-    [SerializeField] private Transform _targetObj;
     // Start is called before the first frame update
+    [SerializeField] 
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        _mouse = Input.mousePosition;
-        _target = Camera.main.ScreenToWorldPoint(new Vector3(_mouse.x, _mouse.y, 10));
-        _targetObj.position = _target;
+        //MouseTes();
     }
+
+    public void OnMouseDown()
+    {
+
+    }
+
+    public void OnMouseUp()
+    {
+
+    }
+
+
+
 }
